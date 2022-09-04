e6f2877bb20bbecfd6691e4c780bbb09a381d9aa
Ali Merkouk <ali.merkouk@gmail.com>
2022-09-04 23:25:33 +0200

    last minute

---
+++ category.js
@@ -0,0 +1,39 @@
+
+fetch("https://kea-alt-del.dk/t7/api/categories")
+.then((res)=>res.json())
+.then(collectEach); 
+
+function collectEach(data){
+data.forEach(showCategory); 
+
+}
+
+
+function showCategory(oneCategory){
+
+
+
+
+    const template = document.querySelector("template").content; 
+
+    // Clone it 
+
+    const copy = template.cloneNode(true); 
+
+
+copy.querySelector("a").textContent=oneCategory.category;
+copy.querySelector("a").href= `/Users/alimerkouk/Documents/s2/pdt list/product-list.html?category=${oneCategory.category}`;
+
+
+    // grab parent
+    const parent=document.querySelector("#letter_b ol"); 
+// Append
+
+parent.appendChild(copy);
+
+
+console.log(oneCategory);
+
+
+}
+
